d99b49dd8843685aa8d0d136c63fc98a022018bf
Jacob Phipps <houseofphipps@gmail.com>
2022-08-30 10:29:51 -0600

    mvp

--- Multiply/ViewController.swift
+++ Multiply/ViewController.swift
@@ -14,6 +14,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    var numOneString = ""
+    var numTwoString = ""
+    var numOneDouble = 0.0
+    var numTwoDouble = 0.0
+    var numSolutionDouble = 0.0
+    var numSolutionString = ""
+    
+ 
+    @IBOutlet weak var firstTextEntry: UITextField!
+    
+    @IBOutlet weak var secondTextEntry: UITextField!
+    
+    @IBAction func actionButtonPressed(_ sender: Any) {
+        numOneString = firstTextEntry.text ?? "0"
+        numTwoString = secondTextEntry.text ?? "0"
+        numOneDouble = Double(numOneString) ?? 0.0
+        numTwoDouble = Double(numTwoString) ?? 0.0
+        numSolutionDouble = numOneDouble * numTwoDouble
+        numSolutionString = String(numSolutionDouble)
+        nameSolution.text = numSolutionString
 
+    }
+    @IBOutlet weak var nameSolution: UILabel!
 }
 
